66eebfce1ab991e898e8c274187e41b3b46e4cac
sang0103-05 <ngosang010305@gmail.com>
2026-03-28 12:42:21 +0700

    Thêm dữ liệu vào dự án

---
+++ BaiTapClass_DuAnMau_ThietKeDam/Models/EtabsAPI_Frame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapClass_DuAnMau_ThietKeDam.Models
+{
+    internal class EtabsAPI_Frame
+    {
+    }
+}
