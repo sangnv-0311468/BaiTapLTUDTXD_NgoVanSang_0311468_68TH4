ed2a932a2ce210a1cc890c9c65305a17f4b7fa6c
sang0103-05 <ngosang010305@gmail.com>
2026-03-30 02:33:33 +0700

    tạo dựng dự án

---
+++ 4_BaiTapMVVM/Views/Pages/Plan2DPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4_BaiTapMVVM.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for Plan2DPage.xaml
+    /// </summary>
+    public partial class Plan2DPage : Window
+    {
+        public Plan2DPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
