cc5ba9e73859986edb178a26b6f0ddda7b5c60bd
sang0103-05 <ngosang010305@gmail.com>
2026-03-30 03:15:06 +0700

    Tạo và hoàn thành dự án thành công

---
+++ Bai2_TaoMenuRibbon_Usercontrol_Pages/UserControls/Usermenu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Bai2_TaoMenuRibbon_Usercontrol_Pages.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bai2_TaoMenuRibbon_Usercontrol_Pages.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Usermenu.xaml
+    /// </summary>
+    public partial class Usermenu : UserControl
+    {
+        public Usermenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_KNEtabs_Click(object sender, RoutedEventArgs e)
+        {
+            KetnoiEtabs ketnoiEtabs = new KetnoiEtabs();
+            ketnoiEtabs.Show();
+        }
+
+        private void btn_chisodat_Click(object sender, RoutedEventArgs e)
+        {
+            Khaibaodiachat khaibaodiachat = new Khaibaodiachat();
+            khaibaodiachat.Show();
+        }
+
+        private void btn_VLDaiCoc_Click(object sender, RoutedEventArgs e)
+        {
+            Vatlieudaicoc vatlieudaicoc = new Vatlieudaicoc();
+            vatlieudaicoc.Show();
+        }
+        private void btn_VLCoc_Click(object sender, RoutedEventArgs e)
+        {
+            Vatlieucoc thepcoc = new Vatlieucoc();
+            thepcoc.Show();
+
+        }
+    }
+}
---
+++ Bai2_TaoMenuRibbon_Usercontrol_Pages/Views/KetnoiEtabs.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bai2_TaoMenuRibbon_Usercontrol_Pages.Views
+{
+    /// <summary>
+    /// Interaction logic for KetnoiEtabs.xaml
+    /// </summary>
+    public partial class KetnoiEtabs : Window
+    {
+        public KetnoiEtabs()
+        {
+            InitializeComponent();
+
+            // Đợi Window load xong rồi mới bắt đầu chạy thanh tiến trình
+            this.Loaded += KetnoiEtabs_Loaded;
+        }
+
+        private async void KetnoiEtabs_Loaded(object sender, RoutedEventArgs e)
+        {
+            await RunFakeConnection();
+        }
+
+        private async Task RunFakeConnection()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                // Cập nhật giá trị thanh ProgressBar
+                pgBar.Value = i;
+
+                // Tăng tốc độ hoặc giảm tùy ý (50ms mỗi 1%)
+                await Task.Delay(1);
+
+                // Khi chạy xong thì đóng hoặc làm gì đó
+                if (i == 100)
+                {
+                    lblStatus.Text = "Kết nối thành công!";
+                    await Task.Delay(500); // Đợi xíu cho người dùng kịp nhìn
+                    this.Close();
+                }
+            }
+        }
+    }
+    
+}
